12e4c1e03d057d2d4c5f38570180c5f819f8169c
michaelpaiml <31094140+michaelpaiml@users.noreply.github.com>
2018-07-09 14:43:17 +0800

    Fixed

---
+++ DebuggingExercises/MovieQuote.java
@@ -0,0 +1,7 @@
+public class MovieQuote
+{
+    public static void main(String[] args)
+    {
+        System.out.println("You Shall Not Pass");
+    }
+}
---
+++ DebuggingExercises/MovieQuoteInfo.java
@@ -0,0 +1,10 @@
+public class MovieQuoteInfo
+{
+    public static void main(String[] args)
+    {
+        System.out.println("You Shall Not Pass");
+        System.out.println("The Lord of the Rings: The Fellowship of the Ring");
+        System.out.println("Gandalf");
+        System.out.println("2001");
+    }
+}
